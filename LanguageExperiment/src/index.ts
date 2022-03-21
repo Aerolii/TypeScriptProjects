6f1e06c1fef3e841f8478d9f01acfc310f8ec562
zhangxiaobo <zhangxiaobo@yunji.ai>
2022-03-21 19:37:59 +0800

    config

---
+++ LanguageExperiment/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: zxb
+ * @Date: 2022-03-18 18:12:35
+ * @LastEditTime: 2022-03-18 18:12:36
+ * @LastEditors: zxb
+ * @Description: 入口函数
+ * @FilePath: /TypeScriptProjects/LanguageExperiment/src/index.ts
+ */
+
+const h = 'Hello TypeScript'
+console.log('h', h)
