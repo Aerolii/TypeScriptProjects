ecded67e6951047d8e0068c9386d1e5a38cae3e3
zhangxiaobo <zhangxiaobo@yunji.ai>
2022-12-06 09:47:47 +0800

    feat: function optionals param

--- src/lesson-1/6.optionals.ts
+++ src/lesson-1/6.optionals.ts
@@ -1,3 +1,5 @@
+// 函数可选参数的使用
+
 function printString(str1: string, str2: string, extra?: string): string {
   return `${str1} ${str2} ${extra || ''}`
 }
@@ -10,3 +12,21 @@ interface User {
     email?: string
   }
 }
+
+function getEmail(user: User): string {
+  if (user.info) {
+    return user.info.email!
+  }
+  return ''
+}
+
+function getEmailEasy(user: User): string {
+  return user.info?.email ?? ''
+}
+
+function addWidthCallback(x: number, y: number, callback?: () => void) {
+  console.log([x, y])
+  callback?.()
+}
+
+console.log(getEmailEasy({ id: 1, info: {} })) // ''
