82ea6f6f441430efd5d00e1403bb73419d6e400e
zhangxiaobo <zhangxiaobo@yunji.ai>
2022-12-13 10:39:28 +0800

    feat: utility types => ConstructorParameters & InstanceType

--- src/lesson-3/7.utility-2.ts
+++ src/lesson-3/7.utility-2.ts
@@ -1,4 +1,9 @@
 // 实用类型
+// Parameters
+// ReturnType
+
+// ConstructorParameters
+// InstanceType
 
 interface Name {
   first: string
@@ -17,8 +22,34 @@ console.log(addFullName({ first: 'Jack', last: 'Harrington' })) // { first: 'Jac
 function permuteRows<T extends (...args: any[]) => any>(
   interatorFunc: T,
   data: Parameters<T>[0][]
-) {
+): ReturnType<T>[] {
   return data.map(interatorFunc)
 }
 
 console.log(permuteRows(addFullName, [{ first: 'Jack', last: 'Harrington' }])) // [ { first: 'Jack', last: 'Harrington', fullName: 'Jack Harrington' } ]
+
+// 在 类 上使用实用类型
+class PersonWithFullName {
+  constructor(public name: Name) {}
+
+  get fullName() {
+    return `${this.name.first} ${this.name.last}`
+  }
+}
+
+const createObjectType = <T extends new (...args: any[]) => any>(
+  ObjectType: T,
+  data: ConstructorParameters<T>[0][]
+): InstanceType<T>[] => {
+  return data.map((item) => new ObjectType(item))
+}
+
+console.log(
+  createObjectType(PersonWithFullName, [{ first: 'tom', last: 'smith' }])
+) // [ PersonWithFullName { name: { first: 'tom', last: 'smith' } } ]
+
+console.log(
+  createObjectType(PersonWithFullName, [{ first: 'tom', last: 'smith' }]).map(
+    (name) => name.fullName
+  )
+) // [ 'tom smith' ]
